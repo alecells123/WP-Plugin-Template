9b4bcdc1d049ed2cb0c793ae9e7d321423ef7c4e
Alec Ellsworth <alec.ells123@gmail.com>
2025-02-20 12:01:51 -0600

    Initial release build

--- wp-plugin-template.php
+++ wp-plugin-template.php
@@ -79,4 +79,45 @@ function run_wp_plugin_template() {
 	$plugin->run();
 
 }
+
+/**
+ * Plugin updater handler function.
+ * Pings the Github repo that hosts the plugin to check for updates.
+ */
+function wp_plugin_template_check_for_plugin_update( $transient ) {
+    // If no update transient or transient is empty, return.
+    if ( empty( $transient->checked ) ) {
+        return $transient;
+    }
+
+    // Plugin slug, path to the main plugin file, and the URL of the update server
+    $plugin_slug = 'wp-plugin-template/wp-plugin-template.php';
+    $update_url = 'https://raw.githubusercontent.com/alecells123/WP-Plugin-Template/refs/heads/main/update-info.json';
+
+    // Fetch update information from your server
+    $response = wp_remote_get( $update_url );
+    if ( is_wp_error( $response ) ) {
+        return $transient;
+    }
+
+    // Parse the JSON response (update_info.json must return the latest version details)
+    $update_info = json_decode( wp_remote_retrieve_body( $response ) );
+
+    // If a new version is available, modify the transient to reflect the update
+    if ( version_compare( $transient->checked[ $plugin_slug ], $update_info->new_version, '<' ) ) {
+        $plugin_data = array(
+            'slug'        => 'wp-plugin-template',
+            'plugin'      => $plugin_slug,
+            'new_version' => $update_info->new_version,
+            'url'         => $update_info->url,
+            'package'     => $update_info->package, // URL of the plugin zip file
+        );
+        $transient->response[ $plugin_slug ] = (object) $plugin_data;
+    }
+
+    return $transient;
+}
+add_filter( 'pre_set_site_transient_update_plugins', 'wp_plugin_template_check_for_plugin_update' );
+
+// Run the plugin.
 run_wp_plugin_template();
